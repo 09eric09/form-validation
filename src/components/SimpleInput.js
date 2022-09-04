04314e0f6e2efe1cc42bd0592fd290133a4cd9fd
09eric09 <09eric09@gmail.com>
2022-09-03 22:30:01 -0700

    added validation for empty state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [inputValue, setInputValue] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const onChangeHandler = (e) => {
     setInputValue(e.target.value);
@@ -9,15 +10,23 @@ const SimpleInput = (props) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    if (inputValue.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
     console.log(inputValue);
     setInputValue('');
   }
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={submitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input onChange={onChangeHandler} type='text' id='name' value={inputValue}/>
+        { !enteredNameIsValid && <p className={'error-text'}>Name must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
